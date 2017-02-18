3019e86eb4fc1bf708588a06d2cac5a0aa3b283e
Akhil Waghmare <Akhil@Akhils-MacBook-Pro.local>
2017-02-18 01:16:32 -0500

    Initial commit

---
+++ Medium Clone/Medium Clone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Medium Clone
+//
+//  Created by Akhil Waghmare on 2/17/17.
+//  Copyright © 2017 AW Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
